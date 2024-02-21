990d8920ffc56e3ffe0eb1bec0ebad880d805e55
Buditanrim <1830184+buditanrim@users.noreply.github.com>
2024-02-21 21:45:22 +0700

    episode-2

--- App.js
+++ App.js
@@ -1,31 +1,33 @@
-/**
- * <div id="parent">
- *  <div id="child">
- *      <h1></h1>
- *  </div>
- * </div>
- * 
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement('div', { id: "parent" },
-    React.createElement('div', { id: "child" }, [
-        React.createElement('h1', {}, "I'm heading 1"),
-        React.createElement('h2', {}, "I'm heading 2")
-    ]
+// React Element
+const jsxHeader = (
+	<div>
+		<h1 className="hai">
+			Hello this is a JSX
+		</h1>
+		<p>aaa</p>
+	</div>
+)
 
-    )
+const Name = () => (
+	<p>Myeon</p>
+)
+
+const pet = 'Mija'
+
+const Heading = () => (
+	<div>
+		<h2>{pet}</h2>
+		{Name()}
+		<h1>
+			<Name />
+			Budi Tanrim Functional Component
+		</h1>
+	</div>
 )
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(parent)
 
-/*
-// under the hood, this creates a React element (Javascript object)
-const heading = React.createElement('h1', { id: "heading" }, 'Hello World from React')
-
-// Tell React the root - using react-dom API
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-// Take the object and put it into the DOM
-root.render(heading)
-*/
+root.render(<Heading />)
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # React
+
+---
+
+# Parcel
+
+parceljs.org
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - faster builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling
+- Diagnostic
+- Error Handling
+- Host HTTPS
+- Tree shaking - remove unused files
